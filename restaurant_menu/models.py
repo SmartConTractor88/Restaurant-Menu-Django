ceacfec425a04fd762d6249e495baa881e074af4
SmartConTractor <login.blockchain888@gmail.com>
2025-03-27 13:03:42 +0100

    Create database.

--- restaurant_menu/models.py
+++ restaurant_menu/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+MEAL_TYPE = (("starters","Starters"),
+             ("salads","Salads"),
+             ("main_dishes","Main Dishes"),
+             ("desserts","Desserts"))
+
+STATUS = ((0, "Unavailable"),
+          (1, "Available"))
+
+class Item(models.Model):
+    meal = models.CharField(max_length=1000, unique=True) # no duplicates
+    description = models.CharField(max_length=2000, unique=False) # different meals can have same ingredients
+    price = models.DecimalField(max_digits=20, decimal_places=2)
+    meal_type = models.CharField(max_length=200, choices=MEAL_TYPE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    status = models.IntegerField(choices=STATUS, default=0) # unavailable by default
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.meal
