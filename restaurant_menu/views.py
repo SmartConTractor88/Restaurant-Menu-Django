305a73b20b6120e38885ce5ee3cc247316755bf9
SmartConTractor <login.blockchain888@gmail.com>
2025-03-27 15:22:00 +0100

    Create context.

--- restaurant_menu/views.py
+++ restaurant_menu/views.py
@@ -8,6 +8,9 @@ class MenuListView(generic.ListView):
     queryset = Item.objects.order_by("-date_created")
     template_name = "index.html"
 
+    def get_context_data(self, **kwargs):
+        context = {"meals":"Pizza", "ingredients":"Cheese"}
+        return context
 
 class MenuItemDetail(generic.DetailView):
     
